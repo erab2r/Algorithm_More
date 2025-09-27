3c342c080ffa35c7853b20578aacd4110fa0fd1e
erab2r <shkera213@gmail.com>
2025-09-28 02:14:01 +0600

    4

--- knapSack/count_of_subsetSum.cpp
+++ knapSack/count_of_subsetSum.cpp
@@ -4,20 +4,20 @@ using namespace std;
 int val[1005];
 int dp[1005][1005];
 
-bool subset_sum(int i, int sum) {
+int subset_sum(int i, int sum) {
     if(i<0)
     {
-        if(sum== 0) return true;
-        else return false;
+        if(sum== 0) return 1;
+        else return 0;
     }
 
     if (dp[i][sum] != -1) 
         return dp[i][sum];
 
     if (val[i] <= sum) {
-        bool op1 = subset_sum(i - 1, sum - val[i]);
-        bool op2 = subset_sum(i - 1, sum);
-        return dp[i][sum] = op1 || op2;
+        int op1 = subset_sum(i - 1, sum - val[i]);
+        int op2 = subset_sum(i - 1, sum);
+        return dp[i][sum] = op1 + op2;
     } else {
         return dp[i][sum] = subset_sum(i - 1, sum);
     }
@@ -35,10 +35,8 @@ int main() {
         for (int j = 0; j <= sum; j++)
             dp[i][j] = -1;
 
-    if (subset_sum(n - 1, sum))
-        cout << "Yes\n";
-    else
-        cout << "No\n";
+        cout << subset_sum(n - 1, sum) <<endl;
+   
 
     return 0;
 }
