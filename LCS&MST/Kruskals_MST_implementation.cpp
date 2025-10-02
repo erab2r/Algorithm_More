d1c9ebb29b6da6fdc5373643759946b012eeacda
erab2r <shkera213@gmail.com>
2025-10-03 04:18:36 +0600

    1

--- LCS&MST/Kruskals_MST_implementation.cpp
+++ LCS&MST/Kruskals_MST_implementation.cpp
@@ -1,5 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+int par[1005];
+int group_size[1005];
+int find(int node)  
+{
+    if(par[node] == -1) 
+        return node;
+    int leader = find(par[node]);
+    par[node] = leader;
+    return leader;
+}
+void dsu_union(int node1,int node2)
+{
+    int leader1 = find(node1);
+    int leader2 = find(node2);
+    if(group_size[leader1] > group_size[leader2])
+    {
+        par[leader2] = leader1;
+         group_size[leader1] += group_size[leader2];
+    }
+    else
+    {
+         par[leader1] = leader2;
+        group_size[leader2] += group_size[leader1];
+    }
+}
 class Edge
 {   public:
         int a,b,c;
@@ -16,6 +41,8 @@ bool cmp(Edge l,Edge r)
 }
 int main()
 {   
+    memset(par,-1,sizeof(par));
+    memset(group_size,1,sizeof(group_size));
     int n,e;
     cin >> n >> e;
     vector<Edge> edge_list;
@@ -26,10 +53,18 @@ int main()
         edge_list.push_back(Edge(a,b,c));
     }
     sort(edge_list.begin(),edge_list.end(),cmp);
+    int totalCost = 0;
     for(auto ed : edge_list)
     {
-        cout << ed.a << " " << ed.b << " " << ed.c << endl;
+        int parA = find(ed.a);
+        int parB = find(ed.b);
+        if(parA != parB)
+        {
+            dsu_union(ed.a,ed.b);
+            totalCost += ed.c;
+        }
     }
+    cout << totalCost <<endl;
     
     return 0;
 }
